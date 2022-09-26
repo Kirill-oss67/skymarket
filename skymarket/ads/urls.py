112b5674e0f443446d133419c55442eeb6dd5673
Kirill-oss67 <Yakovkirill-roslavl@yandex.ru>
2022-09-26 14:07:59 +0300

    progress of implementation

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -6,13 +6,13 @@ from .views import AdViewSet, CommentViewSet
 ads_router = routers.SimpleRouter()
 ads_router.register(r"ads", AdViewSet)
 ads_router.register("ads", AdViewSet, basename="users")
-
-comments_router = routers.SimpleRouter()
-comments_router.register(r"comments", CommentViewSet, basename="comments")
-
-
+#
+# comments_router = routers.SimpleRouter()
+# comments_router.register(r"comments", CommentViewSet, basename="comments")
+#
+#
 urlpatterns = [
-    path("", include(ads_router.urls)),
-    path("", include(comments_router.urls)),
-
+    # path("", include(ads_router.urls)),
+#     path("", include(comments_router.urls)),
+#
 ]
--- skymarket/users/urls.py
+++ skymarket/users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import include, path
 from djoser.views import UserViewSet
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 users_router = SimpleRouter()
 
 users_router.register("users", UserViewSet, basename="users")
 
 urlpatterns = [
-    path("", include(users_router.urls))
+    path("", include(users_router.urls)),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
