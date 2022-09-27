8412031a28397bfbd8a147cbc40e97cfbb63bd55
Kirill-oss67 <Yakovkirill-roslavl@yandex.ru>
2022-09-27 17:43:16 +0300

    add

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -4,15 +4,13 @@ from rest_framework import routers
 from .views import AdViewSet, CommentViewSet
 
 ads_router = routers.SimpleRouter()
-ads_router.register(r"ads", AdViewSet)
 ads_router.register("ads", AdViewSet, basename="users")
-#
-# comments_router = routers.SimpleRouter()
-# comments_router.register(r"comments", CommentViewSet, basename="comments")
-#
-#
+
+comments_router = routers.SimpleRouter()
+comments_router.register("comments", CommentViewSet, basename="comments")
+
 urlpatterns = [
-    # path("", include(ads_router.urls)),
-#     path("", include(comments_router.urls)),
-#
+    # path("api/", include(ads_router.urls)),
+    # path("", include(comments_router.urls)),
+
 ]
